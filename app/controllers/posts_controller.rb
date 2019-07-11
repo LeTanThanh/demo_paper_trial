962fd8a085f937a5a8d4da2f42e9d20348d50603
thanhlt-1007 <le.tan.thanh@sun-asterisk.com>
2019-07-11 21:19:46 +0700

    add_posts#show

--- app/controllers/posts_controller.rb
+++ app/controllers/posts_controller.rb
@@ -1,5 +1,19 @@
 class PostsController < ApplicationController
+  before_action :load_post, only: %i(show)
+ 
   def index
     @posts = Post.all
+  end
+
+  def show
+    @versions = @post.versions
+  end
+
+  private
+
+  def load_post
+    @post = Post.find_by id: params[:id]
+
+    redirect_to root_url unless @post
   end
 end
