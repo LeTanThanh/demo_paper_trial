ade2f240adb234621b8cc178e723e476ef0bb59c
thanhlt-1007 <le.tan.thanh@sun-asterisk.com>
2019-07-11 18:10:38 +0700

    add_post

--- Gemfile
+++ Gemfile
@@ -15,6 +15,7 @@ gem "turbolinks", "~> 5"
 gem "jbuilder", "~> 2.5"
 gem "tzinfo-data", platforms: [:mingw, :mswin, :x64_mingw, :jruby]
 gem "paper_trail"
+gem "ffaker"
 
 group :development, :test do
   gem "byebug", platforms: [:mri, :mingw, :x64_mingw]
--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,14 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20190711105341) do
+ActiveRecord::Schema.define(version: 20190711110452) do
+
+  create_table "posts", force: :cascade do |t|
+    t.string "title"
+    t.text "content"
+    t.datetime "created_at", null: false
+    t.datetime "updated_at", null: false
+  end
 
   create_table "versions", force: :cascade do |t|
     t.string "item_type", null: false
